c72890acaa042aa0641e3d859ccf87c434201671
LastOne817 <lastone817@naver.com>
2015-12-24 11:52:17 +0900

    타일 기초 구성
    빛 반사에 필요한 정도로만 구현

---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hexagon
+{
+    int x, y;   // 맵상 좌표
+    /* <x,y>
+     *   <0,0> <1,0> ...
+     * <1,0> <1,1> ...
+    **/
+    TileType tile;
+    Direction dir;
+
+    public Hexagon(TileType tile, Direction dir)
+    {
+        this.tile = tile;
+        this.dir = dir;
+    }
+
+    public Direction Reflect(Direction input)
+    {
+        return DegreeToDirection(DirectionToDegree(dir) * 2 - DirectionToDegree(input));
+    }
+
+    public static Direction DegreeToDirection(int degree)
+    {
+        degree = (degree % 360);    // Over 360
+        degree = (degree < 0) ? 360 - ((-degree) % 360) : degree; // Under 0
+        switch (degree)
+        {
+            case 0:
+                return Direction.North;
+            case 30:
+                return Direction.NNE;
+            case 60:
+                return Direction.NEE;
+            case 90:
+                return Direction.East;
+            case 120:
+                return Direction.EES;
+            case 150:
+                return Direction.ESS;
+            case 180:
+                return Direction.South;
+            case 210:
+                return Direction.SSW;
+            case 240:
+                return Direction.SWW;
+            case 270:
+                return Direction.West;
+            case 300:
+                return Direction.WWN;
+            case 330:
+                return Direction.WNN;
+            default:
+                return Direction.Exn;
+        }
+    }
+
+    public static int DirectionToDegree(Direction dir)
+    {
+        switch (dir)
+        {
+            case Direction.North:
+                return 0;
+            case Direction.NNE:
+                return 30;
+            case Direction.NEE:
+                return 60;
+            case Direction.East:
+                return 90;
+            case Direction.EES:
+                return 120;
+            case Direction.ESS:
+                return 150;
+            case Direction.South:
+                return 180;
+            case Direction.SSW:
+                return 210;
+            case Direction.SWW:
+                return 240;
+            case Direction.West:
+                return 270;
+            case Direction.WWN:
+                return 300;
+            case Direction.WNN:
+                return 330;
+            default:
+                return 360;
+        }
+    }
+}
+
+public enum TileType { Empty, HalfCorner, HalfEdge, FullCorner, FullEdge }  // Half : 반거울, Full : 거울 / Corner : 꼭지점을 잇는 거울, Edge : 변의 중심을 잇는 거울
+public enum Direction
+{
+    Exn = 0,
+    North,
+    NNE,
+    NEE,
+    East,
+    EES,
+    ESS,
+    South,
+    SSW,
+    SWW,
+    West,
+    WWN,
+    WNN
+}
