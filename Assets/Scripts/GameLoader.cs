64e87d091fc0df63c66cf10c26847b35c2f99015
LastOne817 <lastone817@naver.com>
2015-12-26 17:31:49 +0900

    계산 대충해둔것 수정, InputHandler 구현 WIP

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -7,9 +7,9 @@ public class GameLoader : MonoBehaviour
     public Map map;
     public GameObject[] tiles;
     private Transform mapHolder;
-    private const float scale = 2f;
-    private const float xOffset = -1f;
-    private const float yOffset = -2f;
+    public const float scale = 2f;
+    public const float xOffset = -1f;
+    public const float yOffset = -2f;
 
     // Use this for initialization
     void Start()
@@ -26,7 +26,7 @@ public class GameLoader : MonoBehaviour
 
     Vector3 PosToWorld(Pos p)
     {
-        return new Vector3(xOffset + p.x * 2.33f / 3f, yOffset + ((p.x % 2 == 0) ? p.y : p.y + 0.5f) * Mathf.Sqrt(3f) / 2f, 0f) * scale;
+        return new Vector3(xOffset + p.x * 0.75f, yOffset + ((p.x % 2 == 0) ? p.y : p.y + 0.5f) * Mathf.Sqrt(3f) / 2f, 0f) * scale;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour
+{
+    private Hexagon clicked, released;
+    private MouseStatus status = MouseStatus.Neutral;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Hexagon tile;
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (status == MouseStatus.Clicked)
+            {
+                // Release
+                if(FindObjectOfType<GameLoader>().map.tileset.TryGetValue(WorldToPos(mousePos), out released))
+                {
+                    if (clicked != released)
+                    {
+
+                    }
+                }
+            }
+            status = MouseStatus.Neutral;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(FindObjectOfType<GameLoader>().map.tileset.TryGetValue(WorldToPos(mousePos), out clicked))
+            {
+                // 선택
+            }
+        }
+    }
+
+    Pos WorldToPos(Vector3 input)
+    {
+        // Calculate precise position
+        return new Pos((int)input.x, (int)input.y);
+    }
+}
+
+public enum MouseStatus { Neutral, Clicked }
