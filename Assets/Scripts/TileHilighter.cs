246e1043e378aa38d2331fba9c871538298285e2
wrrho <hahi3347@gmail.com>
2016-02-15 21:12:06 +0900

    3D 모델, 매터리얼, 하일라잇 스크립트 추가

---
+++ Assets/Scripts/TileHilighter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileHilighter : MonoBehaviour {
+    public Material tileMat;
+    public Material hilightMat;
+
+    public void Hilight()
+    {
+        GetComponent<MeshRenderer>().material = hilightMat;
+    }
+
+    public void Dehilight()
+    {
+        GetComponent<MeshRenderer>().material = tileMat;
+    }
+
+}
